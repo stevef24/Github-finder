e9fe484fa8a0aab841e9a82e9d616ee695753c71
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-03-12 22:31:11 +0000

    added about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const About = () => {
-	return <div>About</div>;
+	return (
+		<>
+			<h1 className="text-6xl mb-4">Github Finder</h1>
+			<p className="mb-4 text-2xl font-light">
+				A React app to search GitHub profiles and see profile details. This app
+				was created to familiarise my self with useReducer and useContext hooks
+				in react as well using tailwind css and Daisy UI libaries.
+				<br />
+				<a href=""> Link to my github: </a>
+				<strong>
+					<a href="https://github.com/stevef24"> Stavf24</a>
+				</strong>
+				.
+			</p>
+
+			<p className="text-lg text-gray-400">
+				styled with: Tailwind CSS and Daisey UI
+			</p>
+		</>
+	);
 };
 
 export default About;
